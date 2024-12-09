d868d2581ed7d827326cceeedec7b402caa898a4
Smohanta23 <mohantareet@gmail.com>
2024-12-09 13:43:38 +0530

    Initial commit: Upload Library Management System project

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> E-LMS Portal |<a href="https://github.com/Smohanta23" target="_blank"  > Crafted by : S. Mohanta</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
